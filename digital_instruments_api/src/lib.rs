9094851ebee1a33c4b68adfe9001deb5779446f2
Alan Zhou <alanzhou@client-10-229-148-57.tamulink.tamu.edu>
2023-10-11 18:47:59 -0500

    Changed pitch to 880 Hz

--- digital_instruments_api/src/lib.rs
+++ digital_instruments_api/src/lib.rs
@@ -47,7 +47,7 @@ where
     let mut sample_clock = 0f32;
     let mut next_value = move || {
         sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
+        (sample_clock * 880.0 * 2.0 * 3.141592 / sample_rate).sin()
     };
 
     let err_fn = |err| console::error_1(&format!("an error occurred on stream: {}", err).into());
