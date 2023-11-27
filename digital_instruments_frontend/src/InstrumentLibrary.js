bd8d5f30012610a9fd9925d6b9b90c29ba7b3247
Nurivan Gomez <nurivan.gomez@gmail.com>
2023-11-27 00:11:20 -0600

    More improvements

--- digital_instruments_frontend/src/InstrumentLibrary.js
+++ digital_instruments_frontend/src/InstrumentLibrary.js
@@ -1,6 +1,76 @@
 let InstrumentLibrary = {
     currentInstrument: "None",
-    instruments: {}
+    instruments: {
+      "Demo Instrument": {
+          "title": "Demo Instrument",
+          "img": "https://digitalinstruments.app/instruments.jpg",
+          "description": "This instrument can play notes with the middle row of your keyboard.",
+          "instrumentSound": {
+              "overtoneRelativeAmplitudes": [
+                  0.5,
+                  1,
+                  0.5,
+                  0.5,
+                  0.5
+              ],
+              "amplitudeValues": [
+                  1,
+                  0.01,
+                  1,
+                  0.0446,
+                  0.5768,
+                  0.030700000000000005
+              ]
+          },
+          "binds": [
+              {
+                  "action": "KeyA",
+                  "type": "note",
+                  "value": "F2"
+              },
+              {
+                  "action": "KeyS",
+                  "type": "note",
+                  "value": "F#2"
+              },
+              {
+                  "action": "KeyD",
+                  "type": "note",
+                  "value": "G2"
+              },
+              {
+                  "action": "KeyF",
+                  "type": "note",
+                  "value": "G#2"
+              },
+              {
+                  "action": "KeyG",
+                  "type": "note",
+                  "value": "A2"
+              },
+              {
+                  "action": "KeyH",
+                  "type": "note",
+                  "value": "A#2"
+              },
+              {
+                  "action": "KeyJ",
+                  "type": "note",
+                  "value": "B2"
+              },
+              {
+                  "action": "KeyK",
+                  "type": "note",
+                  "value": "C3"
+              },
+              {
+                  "action": "KeyL",
+                  "type": "note",
+                  "value": "C#3"
+              }
+          ]
+      }
+  }
 };
 
 let updateCallbacks = [];
